49082bfccbcb64890e0194922330a8e3133b8d37
pablo-pacheco <pablo.pacheco@origgami.com.br>
2016-11-08 23:46:36 -0200

    beginning

---
+++ Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WCSON;
+
+class Order extends Design_Pattern\Autoload{
+
+	public function search_by_custom_number($query){
+		if(!is_admin()){
+			return;
+		}
+		if(!isset($query->query)){
+			return;
+		}
+		if(!isset($query->query['s'])){
+			return;
+		}
+		if(filter_var($query->query['s'], FILTER_VALIDATE_INT) === false){
+			return;
+		}
+		$customSearchEnabled = filter_var(get_option('wcj_order_numbers_enabled'),FILTER_VALIDATE_BOOLEAN);
+		if(!$customSearchEnabled || $customSearchEnabled==null){
+			return;				
+		}
+		$custom_order_id = intval($query->query['s']);
+		$query->query_vars['post__in']=array();
+		$query->query['s']='';
+		$query->set('meta_key','_wcj_order_number');
+		$query->set('meta_value',$custom_order_id);
+	}
+}
---
+++ plugin.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Plugin Name: Woocommerce Booster - Search order by custom number (FIX)
+ * Description: Fixes the problem of searching an order by custom number on admin using the plugin <a href="http://booster.io/">"Woocommerce Booster"</a>
+ * Version:     1.0.0
+ * Author:      Origgami
+ * Author URI:  http://origgami.com.br
+ * Depends:     CMB2
+ * License:     GPLv2
+ * Text Domain: wc-booster-search-order-by-custom-number
+ */
+
+namespace WCSON;
+
+//SETUP AUTOLOAD ======================================================================
+spl_autoload_register(function($class) {
+	if ( false !== strpos($class, __NAMESPACE__) ) {
+		if ( !class_exists($class) ) {
+			$dir = plugin_dir_path(__FILE__);
+			$classes_dir = $dir;
+			$class_file = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+			$class_file = str_replace(__NAMESPACE__.DIRECTORY_SEPARATOR, '',$class_file);
+			$file = $classes_dir . $class_file;
+			if ( file_exists($file) ) {
+				require_once $file;
+			}else{
+				error_log( 'ERROR LOADING FILE: '.  print_r($file , true ) );
+			}
+		}
+	}
+});
+
+$order = \WCSON\Order::get_instance();
+add_action('pre_get_posts',array($order,'search_by_custom_number'));
