fa8342226f092c7f7a40f0ff2f35103bb7c0b89a
pablo-pacheco <pablo.sg.pacheco@gmail.com>
2016-11-09 13:56:49 -0200

    Update Order.php
    Now it is possible to search an order id with leading zeros

--- Order.php
+++ Order.php
@@ -14,14 +14,18 @@ class Order extends Design_Pattern\Autoload{
 		if(!isset($query->query['s'])){
 			return;
 		}
-		if(filter_var($query->query['s'], FILTER_VALIDATE_INT) === false){
+		if(is_numeric($query->query['s']) === false){
 			return;
 		}
+		/*if(filter_var($query->query['s'], FILTER_VALIDATE_INT) === false){
+			return;
+		}*/
 		$customSearchEnabled = filter_var(get_option('wcj_order_numbers_enabled'),FILTER_VALIDATE_BOOLEAN);
 		if(!$customSearchEnabled || $customSearchEnabled==null){
 			return;				
 		}
-		$custom_order_id = intval($query->query['s']);
+		//$custom_order_id = intval($query->query['s']);
+		$custom_order_id = $query->query['s'];
 		$query->query_vars['post__in']=array();
 		$query->query['s']='';
 		$query->set('meta_key','_wcj_order_number');
